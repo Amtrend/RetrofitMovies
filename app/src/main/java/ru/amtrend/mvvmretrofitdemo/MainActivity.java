bb11b8cad8825bf567319f9e000ce66472f389e0
Александр <alexandermaltsev@bk.ru>
2021-02-15 18:53:49 +0300

    get data to recycler view

--- app/src/main/java/ru/amtrend/mvvmretrofitdemo/MainActivity.java
+++ app/src/main/java/ru/amtrend/mvvmretrofitdemo/MainActivity.java
@@ -1,17 +1,29 @@
 package ru.amtrend.mvvmretrofitdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import ru.amtrend.mvvmretrofitdemo.adapter.ResultAdapter;
 import ru.amtrend.mvvmretrofitdemo.model.MovieApiResponse;
+import ru.amtrend.mvvmretrofitdemo.model.Result;
 import ru.amtrend.mvvmretrofitdemo.service.MovieApiService;
 import ru.amtrend.mvvmretrofitdemo.service.RetrofitInstance;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Result> results;
+    private RecyclerView recyclerView;
+    private ResultAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<MovieApiResponse> call, Response<MovieApiResponse> response) {
                 MovieApiResponse movieApiResponse = response.body();
+
+                if (movieApiResponse != null && movieApiResponse.getResults() != null) {
+                    results = (ArrayList<Result>) movieApiResponse.getResults();
+                    fillRecyclerView();
+                }
             }
 
             @Override
@@ -38,5 +55,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void fillRecyclerView() {
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new ResultAdapter(this, results);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+            recyclerView.setLayoutManager(new GridLayoutManager(this
+                    , 2));
+
+        } else {
+
+            recyclerView.setLayoutManager(new GridLayoutManager(this
+                    , 4));
+
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
