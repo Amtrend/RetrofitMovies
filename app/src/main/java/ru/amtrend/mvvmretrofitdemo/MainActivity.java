5302ee350cabec422a4d9be6ef16496e4ce98bd7
Александр <alexandermaltsev@bk.ru>
2021-02-15 15:34:29 +0300

    get data from rest api

--- app/src/main/java/ru/amtrend/mvvmretrofitdemo/MainActivity.java
+++ app/src/main/java/ru/amtrend/mvvmretrofitdemo/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import ru.amtrend.mvvmretrofitdemo.model.MovieApiResponse;
+import ru.amtrend.mvvmretrofitdemo.service.MovieApiService;
+import ru.amtrend.mvvmretrofitdemo.service.RetrofitInstance;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPopularMovies();
+    }
+
+    public void getPopularMovies() {
+
+        MovieApiService movieApiService = RetrofitInstance.getService();
+
+        Call<MovieApiResponse> call = movieApiService.getPopularMovies(getString(R.string.api_key));
+        call.enqueue(new Callback<MovieApiResponse>() {
+            @Override
+            public void onResponse(Call<MovieApiResponse> call, Response<MovieApiResponse> response) {
+                MovieApiResponse movieApiResponse = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<MovieApiResponse> call, Throwable t) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/ru/amtrend/mvvmretrofitdemo/service/RetrofitInstance.java
@@ -0,0 +1,23 @@
+package ru.amtrend.mvvmretrofitdemo.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    public static Retrofit retrofit = null;
+    private static String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static MovieApiService getService() {
+
+        if (retrofit == null) {
+
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+        }
+
+        return retrofit.create(MovieApiService.class);
+
+    }
+
+}
