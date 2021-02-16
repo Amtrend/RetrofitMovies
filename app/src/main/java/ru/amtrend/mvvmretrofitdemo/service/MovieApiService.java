9e4db62e57718b1a7ca52054dee52ca478227af2
Александр <alexandermaltsev@bk.ru>
2021-02-16 12:45:59 +0300

    implementation paging library dependensy && setup("page")

--- app/src/main/java/ru/amtrend/mvvmretrofitdemo/service/MovieApiService.java
+++ app/src/main/java/ru/amtrend/mvvmretrofitdemo/service/MovieApiService.java
@@ -10,4 +10,6 @@ public interface MovieApiService {
     @GET("movie/popular")
     Call<MovieApiResponse> getPopularMovies(@Query("api_key") String apiKey);
 
+    @GET("movie/popular")
+    Call<MovieApiResponse> getPopularMoviesWithPaging(@Query("api_key") String apiKey, @Query("page") long page);
 }
